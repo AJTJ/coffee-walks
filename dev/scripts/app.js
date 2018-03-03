e9884d43608142e75e737f6e5e8a4accc6d9c539
Ferdinand Ismael <ferdinandismael@Ferdinands-MacBook-Pro.local>
2018-03-03 16:57:59 -0500

    updating functional map container

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -5,6 +5,7 @@ import axios from "axios";
 import MyWalks from "./MyWalks.js";
 import MapContainer from './MapContainer.js';
 import Directions from './Directions.js';
+import Home from "./Home.js";
 import {
    BrowserRouter as Router,
    Route,
@@ -12,54 +13,23 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
-   constructor() {
-      super();
-      this.state = {
-         nearbyPlaces: [],
-         lat: "",
-         lng: "",
-         testing: false
-      }
-      this.getNearbyPlaces = this.getNearbyPlaces.bind(this);
-      this.getUserAreaLocation = this.getUserAreaLocation.bind(this);
-      this.confirmStart = this.confirmStart.bind(this);
-   }
-
-   getNearbyPlaces(nearbyPlaces) {
-      this.setState({
-         nearbyPlaces: nearbyPlaces
-      })
-   }
-
-   getUserAreaLocation(lat, lng) {
-      this.setState({
-         lat: lat,
-         lng: lng
-      })
-   }
-
-   confirmStart() {
-      // console.log(e);
-      console.log("Murder Fish");
-      // this.setState({
-      //   testing: true
-      // })
-   }
-
-   render() {
-      return (
-         <div className="wrapper">
-            <header className="header clearfix">
-               <h1 className="header__title">Coffee Walks</h1>
-               <img className="header__logo" src="/assets/noun_59078_cc.svg" alt=""/>
-            </header>
-            {/* exact states that the path has to state the path EXACTLY to render in the specific component */}
-            <MyWalks getNearbyPlaces={this.getNearbyPlaces} getUserAreaLocation={this.getUserAreaLocation} />
-            <Directions />
-            <MapContainer nearbyPlaces={this.state.nearbyPlaces} areaLat={this.state.lat} areaLng={this.state.lng} confirmStart={this.confirmStart}/>
-         </div>
-      )
-   }
+  render() {
+    return (
+      <Router>
+        <div>
+          <header>
+            {/* to style and add classnames */}
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/contact">Contact</Link>
+          </header>
+          <Route path="/" exact component={Home} />
+          {/* <Route path="/about" exact component={About} />
+          <Route path="/contact/:name" exact component={Contact} /> */}
+        </div>
+      </Router>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
