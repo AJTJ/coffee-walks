824a79adf986e8ff829d80651b89ba4d347653d0
Ferdinand Ismael <ferdinandismael@Ferdinands-MacBook-Pro.local>
2018-03-05 17:26:51 -0500

    adding routeCard

--- dev/scripts/RouteCard.js
+++ dev/scripts/RouteCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 const RouteCard = (props) => {
    console.log(props);
@@ -6,9 +7,11 @@ const RouteCard = (props) => {
       <section className="routeContainer">
          <div className="route">
             <h2>Starting Location</h2>
+            <h3>{props.data.start.name}</h3>
          </div>
          <div className="route">
             <h2>Ending Location</h2>
+            <h3>{props.data.end.name}</h3>
          </div>
       </section>
    )
