4720b08bf3fd701e5714db418b1c17d861b816a6
Ferdinand Ismael <ferdinandismael@Ferdinands-MacBook-Pro.local>
2018-03-05 14:53:43 -0500

    adding SavedWalksjs

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -8,6 +8,7 @@ import Directions from './Directions.js';
 import Header from "./Header.js";
 import Home from "./Home.js";
 import Login from "./Login.js";
+import SavedWalks from "./SavedWalks.js"
 import {
    BrowserRouter as Router,
    Route,
@@ -15,61 +16,81 @@ import {
 } from "react-router-dom";
 
 class App extends React.Component {
-   constructor() {
-      super();
-      this.state = {
-         loggedIn: false,
-         user: null,
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      user: null
+    };
+
+    this.loggedInCheck = this.loggedInCheck.bind(this);
+  }
+
+  loggedInCheck(res) {
+    console.log("res", res);
+    if (res) {
+      this.setState({
+        loggedIn: true,
+        user: res
+      });
+    } else {
+      this.setState({
+        loggedIn: false,
+        user: {}
+      });
+    }
+  }
+
+  componentDidMount() {
+    const dbref = firebase.database().ref("/recipes");
+    dbref.on("value", snapshot => {
+      console.log(snapshot.val());
+      const data = snapshot.val();
+      const state = [];
+      for (let key in data) {
+        data[key].key = key;
+        state.push(data[key]);
       }
+      console.log(state);
+      this.setState({
+        recipes: state
+      });
+    });
+  }
 
-      this.loggedInCheck = this.loggedInCheck.bind(this);
-   }
-
-   loggedInCheck(res) {
-      console.log('res',res)
-      if (res) {
-         this.setState({
-            loggedIn: true,
-            user: res
-         });
-         } else {
-         this.setState({
-            loggedIn: false,
-            user: {}
-         });
-      }
-   }
-
-   componentDidMount() {
-      console.log(this.state.loggedIn);
-   }
-
-   
-
-
-   render() {
-      return (
-         <Router>
+  render() {
+    return (
+      <Router>
+        <div>
+          <header>
+            <Header />
+          </header>
+          {this.state.loggedIn ? (
             <div>
-               <header>
-                  <Header />
-               </header>
-               {this.state.loggedIn ? (
-                  <div>
-                     <Login loggedInCheck={this.loggedInCheck} loggedIn={this.state.loggedIn} user={this.state.user} />
-                     <Link to="/Home">Home</Link>
-                     <Route path="/Home" exact component={Home} />
-                     <Route path="/Directions" exact component={Directions} />
-                  </div>
-               ) : (
-                  <div>
-                     <Login loggedInCheck={this.loggedInCheck} loggedIn={this.state.loggedIn} user={this.state.user} />
-                  </div>
-               )}
+              <Login
+                loggedInCheck={this.loggedInCheck}
+                loggedIn={this.state.loggedIn}
+                user={this.state.user}
+              />
+              <Link to="/Home">Home</Link>
+              <Link to="/SavedWalks">Saved Walks</Link>
+              <Route path="/Home" exact component={Home} />
+              <Route path="/Directions" exact component={Directions} />
+              <Route path="/SavedWalks" exact component={SavedWalks} />
             </div>
-         </Router>
-      );
-   }
+          ) : (
+            <div>
+              <Login
+                loggedInCheck={this.loggedInCheck}
+                loggedIn={this.state.loggedIn}
+                user={this.state.user}
+              />
+            </div>
+          )}
+        </div>
+      </Router>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
