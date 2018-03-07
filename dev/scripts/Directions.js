32eb9fb02028ec24dbc388e0ca77f6ad3cce5c6c
Aaron Janke <janke.aaron@gmail.com>
2018-03-06 22:25:23 -0500

    stylin it all up

--- dev/scripts/Directions.js
+++ dev/scripts/Directions.js
@@ -25,7 +25,7 @@ class Map extends React.PureComponent {
       const MapWithADirectionsRenderer = compose(
          withProps({
            
-            googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${config.key7}&v=3.exp&libraries=geometry,drawing,places`,
+            googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${config.key3}&v=3.exp&libraries=geometry,drawing,places`,
 
             loadingElement: <div style={{ height: `100%` }} />,
             containerElement: <div style={{ height: `55vh` }} />,
